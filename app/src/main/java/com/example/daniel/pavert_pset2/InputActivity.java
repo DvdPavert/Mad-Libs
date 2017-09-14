5bcbf39e69964c0fef0ff60e2f97407b92ba33dd
D van de Pavert <danielvdpavert@gmail.com>
2017-09-14 12:09:56 +0200

    Finished product

--- app/src/main/java/com/example/daniel/pavert_pset2/InputActivity.java
+++ app/src/main/java/com/example/daniel/pavert_pset2/InputActivity.java
@@ -16,16 +16,15 @@ public class InputActivity extends AppCompatActivity {
 
     private Story story;
     private EditText editWord;
-    private Spinner spinner;
+    private int storyNo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
 
+        storyNo =  getIntent().getIntExtra("StoryNo", 0);
         editWord =  (EditText) findViewById(R.id.editWord);
-        spinner = (Spinner) findViewById(R.id.spinnerStory);
-        addListenerOnSpinnerItemSelection();
 
         try
         {
@@ -37,68 +36,51 @@ public class InputActivity extends AppCompatActivity {
         editWord.setText(story.getNextPlaceholder());
     }
 
-    @Override
-    public void onBackPressed()
+
+    public void getStory() throws IOException
     {
-        Intent intent = new Intent(this, StartActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    public void toStory(View view) throws IOException {
-        Intent intent = new Intent(this, StoryActivity.class);
-
-        Bundle bundle = new Bundle();
-        bundle.putSerializable("Story", story);
-
-        intent.putExtras(bundle);
-
-        startActivity(intent);
-        finish();
-    }
-
-
-    public void addListenerOnSpinnerItemSelection() {
-        spinner.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
-
-    private void getStory() throws IOException
-    {
-        //spinner.getSelectedItem()
-    //// TODO: 12/09/2017 Doe hier iets met de geselecteerde spinner ding 
 
         InputStream targetStream  = null;
-        Random r = new Random();
-        int storyNo = r.nextInt(5);
 
-       switch(storyNo)
-       {
-           case(0):
-           {
-               targetStream = getAssets().open("Stories/madlib0_simple.txt", AssetManager.ACCESS_UNKNOWN);
-               break;
-           }
-           case(1):
-           {
-               targetStream = getAssets().open("Stories/madlib1_tarzan.txt", AssetManager.ACCESS_UNKNOWN);
-               break;
-           }
-           case(2):
-           {
-               targetStream = getAssets().open("Stories/madlib2_university.txt", AssetManager.ACCESS_UNKNOWN);
-               break;
-           }
-           case(3):
-           {
-               targetStream = getAssets().open("Stories/madlib3_clothes.txt", AssetManager.ACCESS_UNKNOWN);
-               break;
-           }
-           case(4):
-           {
-               targetStream = getAssets().open("Stories/madlib4_dance.txt", AssetManager.ACCESS_UNKNOWN);
-               break;
-           }
-       }
+        if(storyNo == -1)
+        {
+            Random r = new Random();
+            storyNo = r.nextInt(5);
+        }
+
+        switch (storyNo)
+        {
+            case(0):
+            {
+                targetStream = getAssets().open("Stories/madlib0_simple.txt", AssetManager.ACCESS_UNKNOWN);
+                break;
+            }
+
+            case(1):
+            {
+                targetStream = getAssets().open("Stories/madlib1_tarzan.txt", AssetManager.ACCESS_UNKNOWN);
+                break;
+            }
+
+            case(2):
+            {
+                targetStream = getAssets().open("Stories/madlib2_university.txt", AssetManager.ACCESS_UNKNOWN);
+                break;
+            }
+
+            case(3):
+            {
+                targetStream = getAssets().open("Stories/madlib3_clothes.txt", AssetManager.ACCESS_UNKNOWN);
+                break;
+            }
+
+            case(4):
+            {
+                targetStream = getAssets().open("Stories/madlib4_dance.txt", AssetManager.ACCESS_UNKNOWN);
+                break;
+            }
+
+        }
 
         story = new Story(targetStream);
     }
@@ -111,7 +93,29 @@ public class InputActivity extends AppCompatActivity {
 
         if(story.isFilledIn())
         {
-            toStory(view);
+            toStory();
         }
     }
+
+    @Override
+    public void onBackPressed()
+    {
+        Intent intent = new Intent(this, StartActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void toStory() throws IOException {
+        Intent intent = new Intent(this, StoryActivity.class);
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("Story", story);
+
+        intent.putExtras(bundle);
+
+        startActivity(intent);
+        finish();
+    }
+
+
 }
--- app/src/main/java/com/example/daniel/pavert_pset2/StartActivity.java
+++ app/src/main/java/com/example/daniel/pavert_pset2/StartActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Spinner;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -16,7 +17,48 @@ public class StartActivity extends AppCompatActivity {
     public void toInput(View view)
     {
         Intent intent = new Intent(this, InputActivity.class);
+
+        int storyNo = getStory();
+        intent.putExtra("StoryNo", storyNo);
         startActivity(intent);
         finish();
+    }
+
+    private int getStory()
+    {
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerStory);
+
+        switch(spinner.getSelectedItem().toString())
+        {
+            case("Random"):
+            {
+                return -1;
+            }
+            case("Simple"):
+            {
+                return 0;
+            }
+            case("Tarzan"):
+            {
+                return 1;
+            }
+            case("University"):
+            {
+                return 2;
+            }
+            case("Clothes"):
+            {
+                return 3;
+            }
+            case("Dance"):
+            {
+                return 4;
+            }
+            default:
+            {
+                return -1;
+            }
+
+        }
     }
 }
